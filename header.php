abbc5733e534c2925744ac853557aea8df736125
Shelly Duggal <shellduggal@gmail.com>
2017-02-11 21:08:34 -0600

    Lecture 4

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Awesome Theme</title>
+
+		<?php wp_head(); ?>
+	</head>
+
+	<?php 
+		if(is_front_page()):
+			$awesome_classes = array('awesome-class', 'my-class');
+		else:
+			$awesome_classes = array('no-awesome-class');
+		endif;
+	?>
+
+	<body <?php body_class($awesome_classes); ?>>
+		<?php wp_nav_menu(array('theme_location'=>'primary')); ?>
