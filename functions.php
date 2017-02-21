a28eab154a5e02686f887a99533d201003bcac8f
Shelly Duggal <shellduggal@gmail.com>
2017-02-21 14:22:04 -0600

    Add Bootstrap & jQuery

--- functions.php
+++ functions.php
@@ -1,13 +1,14 @@
 <?php
 
-/////////////////
-// Stylesheets //
-/////////////////
+/////////////////////
+// Include Scripts //
+/////////////////////
 
 function awesome_script_enqueue() {
 	
 	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
 	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
+	wp_enqueue_script('jquery');
 	
 }
 
--- header.php
+++ header.php
@@ -5,6 +5,9 @@
 
 		<title>Awesome Theme</title>
 
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 		<?php wp_head(); ?>
 	</head>
 
